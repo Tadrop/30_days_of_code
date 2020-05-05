f530f85d6290b2a7729c57d0ed53ad96a18d688f
Tadrop <64656838+Tadrop@users.noreply.github.com>
2020-05-05 02:31:01 +0100

    Add files via upload

--- evenandoddfilter.py
+++ evenandoddfilter.py
@@ -1,12 +1,8 @@
 #Even and Odd Number Filter
 def even_odd(num_list):
     string=[x for x in num_list if x %2==0]
-    return 'Even numbers in the list are ',len(string)
-print(even_odd([1,2,3,4,5,6,7,8,9,10,18]))
-print(even_odd(range(10,15)))
-def even_odd(num_list):
+    print('Even numbers in the list are ',len(string))
     strings=[y for y in num_list if y%2!=0]
-    return 'Odd numbers in the list are', len(strings)
-print(even_odd([1,2,3,4,5,6,7,8,9,10,18]))
-print(even_odd(range(10,15)))
-
+    print( 'Odd numbers in the list are', len(strings))
+even_odd([1,2,3,4,5,6,7,8,9,10,18])
+even_odd(range(10,15))
