8403f673cb874b17595bdfbe2f048fa8a07e150a
Tadrop <64656838+Tadrop@users.noreply.github.com>
2020-05-10 01:29:06 +0100

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,20 @@
+def fibSum(num):
+    if not isinstance(num,int):
+        return  'Invalid input, number must be positive'
+    if num == 0 :
+        return 0
+    elif num < 0:
+        return "Invalid, number can't be negative"
+    elif num > 0:
+        pass
+    number =0
+    a,c=0,1
+    while a>=num:
+        a,c= c, a+c
+        if c%2 == 0 :
+            num+=c
+    return num
+print(fibSum(10))
+print(fibSum(0))
+print(fibSum(-3))
+print(fibSum('a'))
